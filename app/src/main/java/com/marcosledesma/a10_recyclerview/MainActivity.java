3274cea587734d8e355f1a532f2ffeaf50418f6b
Marcos Ledesma <marcos.ledesma.picazo@cieep.com>
2020-12-14 16:25:47 +0100

    Añadido main_menu.xml

--- app/src/main/java/com/marcosledesma/a10_recyclerview/MainActivity.java
+++ app/src/main/java/com/marcosledesma/a10_recyclerview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.marcosledesma.a10_recyclerview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -7,7 +8,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Adapter;
+import android.widget.Toast;
 
 import com.marcosledesma.a10_recyclerview.Adapters.OrdenadoresAdapter;
 import com.marcosledesma.a10_recyclerview.Modelos.Ordenador;
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     // ADAPTER
     private OrdenadoresAdapter adapter;
 
+    private LinearLayoutManager linearLayoutManagerVertical;
+    private GridLayoutManager gridLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,10 +44,10 @@ public class MainActivity extends AppCompatActivity {
         inicializaDatos();
 
         // Permite tener diferentes Layouts (Contenedores -> "estructurantes")
-        LinearLayoutManager linearLayoutManagerVertical = new LinearLayoutManager(this);    // Vertical por defecto
-        LinearLayoutManager linearLayoutManagerHorizontal = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        linearLayoutManagerVertical = new LinearLayoutManager(this);    // Vertical por defecto
+        // LinearLayoutManager linearLayoutManagerHorizontal = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         // Permite elegir cantidad de filas (spanCount) -> Se comportará como un Linear Vertical
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        gridLayoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(linearLayoutManagerVertical);
 
         // Iniciar adapter
@@ -55,5 +63,49 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 1000 ; i++) {
             listaOrdenadores.add(new Ordenador("Marca", "Modelo", 2, 3.5f));
         }
+    }
+
+    // Los 2 métodos que siempre borramos ahora nos hacen falta para el menú
+    // Para montar el menú
+    /**
+     * Para insertar el menú en la actividad (inicializar)
+     * @param menu en el objeto menú del método
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);    // Eliminar el return del super
+
+        // Menú inflater con el resource fail creado (main_menu)
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return true;    // Por último return true
+    }
+
+    // Para opciones del menú
+    /**
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);  // Eliminar el return del super aquí tambien
+
+        // Según el item pulsado
+        switch (item.getItemId()){
+            case R.id.gridView:
+                recyclerView.setLayoutManager(gridLayoutManager);
+                break;
+            case R.id.linearView:
+                recyclerView.setLayoutManager(linearLayoutManagerVertical);
+                break;
+            case R.id.configMenu:
+                Toast.makeText(this, "Has presionado Config", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;    // Al final return true
     }
 }
